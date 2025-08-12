6f3955e65795ddb265de154a951e974ed764ae8a
AIDrivenEducation <aidriveneducation@gmail.com>
2025-08-12 14:47:48 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE_NAME = 'scs-cache-v1';
+const CACHE_NAME = 'scs-cache-v2';
 const CORE = ['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install', (e) => { e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(CORE))); self.skipWaiting(); });
 self.addEventListener('activate', (e) => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k!==CACHE_NAME && caches.delete(k))))); self.clients.claim(); });
